df664b7fda036f39e614d52d835e6bef65d43707
min02choi <99067251+min02choi@users.noreply.github.com>
2024-07-14 17:47:25 +0900

    [#15649] N과 M(1)

---
+++ 15000-15999/15600-15699/15649_N과M(1).py
@@ -0,0 +1,20 @@
+# N과 M(1)
+# Silver3
+# 백트래킹
+
+
+def dfs():
+    if (len(arr) == m):
+        print(" ".join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if (i not in arr):
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+
+n, m = map(int, input().split())
+arr = []
+
+dfs()
