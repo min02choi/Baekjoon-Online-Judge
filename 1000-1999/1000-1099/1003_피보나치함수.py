3066399f9f8e0105a69b11b60d78026faec0e279
min02choi <min02choi@naver.com>
2024-07-22 15:48:24 +0900

    [#1003] 피보나치 함수

--- 1000-1999/1000-1099/1003_피보나치함수.py
+++ 1000-1999/1000-1099/1003_피보나치함수.py
@@ -1,4 +1,14 @@
-# 23.09.25
+# 24.07.22
 # 다이나믹 프로그래밍
 
 
+fib01 = {0: [1, 0],
+         1: [0, 1]}
+
+for i in range(2, 41):
+    fib01[i] = [fib01[i-1][0] + fib01[i-2][0], fib01[i-1][1] + fib01[i-2][1]]
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(fib01[n][0], fib01[n][1])
